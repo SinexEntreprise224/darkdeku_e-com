6e7dd4cff98ed5c2348f96bac81dd360e1d8c6d3
dark_deku <mohamedsinesangare2023@gmail.com>
2026-04-12 21:56:00 +0000

    sixth

--- E_com/settings.py
+++ E_com/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --- Paramètres de Sécurité ---
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 # --- Application definition ---
 INSTALLED_APPS = [
     'django.contrib.admin',
